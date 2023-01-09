ec3589ec03bf28dd5c5cbc285f770a2307dad162
Dmitry Strizhakov <d.strizhakov@gmail.com>
2023-01-09 16:30:45 +0300

    feat: add ActiveLink

--- components/Header.js
+++ components/Header.js
@@ -1,13 +1,13 @@
-import Link from 'next/link';
 import styles from '../styles/header.module.css'
+import ActiveLink from './ActiveLink';
 
 const Header = () => {
 	return (
 		<header className={styles.header}>
 			<img src="/idea.svg" className={styles.icon} />
 			<nav className={styles.nav}>
-				<Link href='/'>Generate ideas</Link>
-				<Link href='/about'>About OpenAI</Link>
+				<ActiveLink activeClassName={styles.active} className={styles.navLink} href='/'>Generate ideas</ActiveLink>
+				<ActiveLink activeClassName={styles.active} className={styles.navLink} href='/about'>About OpenAI</ActiveLink>
 			</nav>
 		</header>
 	)
